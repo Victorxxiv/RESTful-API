1d95d2e17122b84122389d19e4dafa2d6f4451eb
Victor <victormwendwa804@gmail.com>
2024-08-26 13:03:13 +0300

    Requesting task_id

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,25 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
 tasks = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
     },
     {
         'id': 2,
-        'title': u'Learn Python',
-        'description': u'Meed to find a good python tutorial on the web',
+        'title': 'Learn Python',
+        'description': 'Meed to find a good python tutorial on the web',
         'done': False
     }
 ]
 
+# GET /tasks
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+# GET /tasks/<task_id>
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 
 if __name__ == '__main__':
