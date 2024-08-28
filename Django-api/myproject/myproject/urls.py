e19b30bd24f2a08b627767b4fc56e52640ca7c77
Victor <victormwendwa804@gmail.com>
2024-08-28 07:29:05 +0300

    Testing, Debugging, Production, Deployment

--- Django-api/myproject/myproject/urls.py
+++ Django-api/myproject/myproject/urls.py
@@ -31,8 +31,9 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+# url path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),
-     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
