31e7dc089919667767c835fdb7e7377a5ffb8c78
Victor <victormwendwa804@gmail.com>
2024-08-28 07:26:12 +0300

    Documentation with Swagger

--- Django-api/myproject/myproject/settings.py
+++ Django-api/myproject/myproject/settings.py
@@ -41,6 +41,21 @@ INSTALLED_APPS = [
     "myapp",
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
